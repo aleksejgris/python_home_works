35333ec5ba0905f28fd43c7b3b6a6abebc80a89b
Леха <aleksejgris8@gmail.com>
2024-12-30 09:38:00 +0200

    My first commit

---
+++ 03_lesson/lesson_3_task_3.py
@@ -0,0 +1,13 @@
+from mailing import Mailing
+from address import Address
+
+to_address = Address("152138" , "Москва" , "Ленина", "10", "20")
+
+from_address = Address("138152" , "Ленинград", "Ленина", "10" , "20")
+
+
+mailing = Mailing(30,to_address,from_address, "40")
+
+
+print(mailing)
+
