daf33389ce891bc38782f3302ad9bc8336571773
aleksejgris <aleksejgris8@gmail.com>
2025-02-04 21:24:47 +0200

    Update waiting for pictures.py

--- lesson_6/waiting for pictures.py
+++ lesson_6/waiting for pictures.py
@@ -1,13 +1,13 @@
-from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome (service=ChromeService( ChromeDriverManager().install()))
-driver.implicitly_wait(20)
+driver.implicitly_wait(40)
+driver.maximize_window()
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
-sleep(25)
+
 
 abc = driver.find_element(By.CSS_SELECTOR, 'img[scr = "img/award.png"]').text
 print(abc)
