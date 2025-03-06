b40c79b6f92cd9364c6fa265e728f5f39719ea5b
aleksejgris <aleksejgris8@gmail.com>
2025-03-06 20:17:52 +0200

    Delete 07lesson/test_data_form/data_page.py

--- 07lesson/test_data_form/data_page.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-
-class Data_page:
-    def __init__(self,driver):
-        self._driver = browser
