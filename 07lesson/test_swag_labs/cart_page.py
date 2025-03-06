9addbd4914f8c42095c051b5c0ee15c1586fb09d
aleksejgris <aleksejgris8@gmail.com>
2025-03-06 20:18:15 +0200

    Delete 07lesson/test_swag_labs/cart_page.py

--- 07lesson/test_swag_labs/cart_page.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class Cart:
-    def __int__(self,browser):
-        self._driver = browser
-
-    def cart(self):
-        self.driver.find_element(By.ID, "checkout").click()
