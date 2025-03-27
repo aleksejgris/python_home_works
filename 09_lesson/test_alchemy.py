1329ab32f83421164a4571a5b236c20e775db4a7
Леха <aleksejgris8@gmail.com>
2025-03-27 19:25:33 +0200

    test db2

--- 09_lesson/test_alchemy.py
+++ 09_lesson/test_alchemy.py
@@ -12,17 +12,18 @@ def test_db_connection():
 
 def test_insert():
       db = create_engine(db_connection_sting)
-      sql= text("insert  into student user_id" "education_form values ('1255','group')")
-      db.delete('1255','group')
+      sql= text("INSERT  INTO student user_id" "education_form VALUES ('1255','group')")
+      db.execute(sql,new_id = 1255,new_form ='group')
 
 def test_update():
       db = create_engine(db_connection_sting)
-      sql = text("insert  into student user_id" "education_form values ('1255','group')")
-      sql = text("update student set education_form = 'null' where user_id = 1255)")
-      db.delete('1255','null')
+      sql = text("INSERT  INTO student user_id" "education_form VALUES ('1255','group')")
+      sql = text("update student set education_form = 'null' where user_id = 1255")
+      db.execute(sql,education_form = 'null',user_id =1255 )
 
 
 def test_delete():
       db = create_engine(db_connection_sting)
-      sql = text("insert  into student user_id" "education_form values ('1255','group')")
+      sql = text("INSERT  INTO student user_id" "education_form VALUES ('1255','group')")
       sql = text(" delete from student where user_id = 1255")
+      db.execute(sql,user_id = 1255)
